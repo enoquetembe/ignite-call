e1ff86e92d53b2757c446e5ee23c73193f1a3758
enoquetembe <enoquetembetembe10@gmail.com>
2023-08-25 15:47:46 +0200

    feat: finishing schedule page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,11 @@
-import { globalStyles } from '@/styles/global'
+import '../lib/dayjs'
+
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { globalStyles } from '@/styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -13,6 +16,15 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.com',
+            siteName: 'Ignite Call',
+          }}
+        />
+
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
